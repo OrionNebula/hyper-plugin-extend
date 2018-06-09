10665ae0e1129f502952b3d1d8600520cb0c6bcb
Olivia Trewin <zancoproductions@gmail.com>
2018-06-08 23:54:29 -0500

    Added some more specific type information

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,18 @@
  * @author Olivia Trewin <zancoproductions@gmail.com>
  */
 
+export interface PluginConstructor {
+  new (...args: any[]): any
+}
+
 /**
  * Register a sub-plugin that extends the functionality of another plugin.
  * @param {string} parentName - The name used by the parent plugin when asking for sub-plugins.
- * @param {Function} subPluginClass - A class that is provided to the parent plugin.
- * @param {Function?} extraCode - A callback that is executed after sub-plugin registration.
- * @returns {(window: any) => void}
+ * @param {PluginConstructor} subPluginClass - A class that is provided to the parent plugin.
+ * @param {(window: Window) => void?} extraCode - A callback that is executed after sub-plugin registration.
+ * @returns {(window: Window) => void}
  */
-function registerSubPlugin (parentName: string, subPluginClass: Function, extraCode?: Function): (window: any) => void {
+export function registerSubPlugin (parentName: string, subPluginClass: PluginConstructor, extraCode?: (window: Window) => void): (window: any) => void {
   return function (window) {
     if (!window.hyper_plugin_extend) {
       window.hyper_plugin_extend = {}
@@ -35,10 +39,10 @@ function registerSubPlugin (parentName: string, subPluginClass: Function, extraC
 /**
  * Register yourself as a parent plugin.
  * @param {string} pluginName - The name sub-plugins are registered under.
- * @param {Function?} onRegister - A callback executed for each sub-plugin that's registered.
- * @returns {(window: any) => void}
+ * @param {(PluginClass: PluginConstructor) => void?} onRegister - A callback executed for each sub-plugin that's registered.
+ * @returns {(window: Window) => void}
  */
-function registerParentPlugin (pluginName: string, onRegister?: Function): (window: any) => void {
+export function registerParentPlugin (pluginName: string, onRegister?: (PluginClass: PluginConstructor) => void): (window: any) => void {
   return function (window) {
     if (!window.hyper_plugin_extend) {
       window.hyper_plugin_extend = {}
@@ -60,9 +64,9 @@ function registerParentPlugin (pluginName: string, onRegister?: Function): (wind
  * Get all sub-plugins registered under the specified name.
  * @param {string} pluginName - All plugins under this name are retrieved.
  * @param {Window?} wnd - A reference to window.
- * @returns {Function[]}
+ * @returns {PluginConstructor[]}
  */
-function getSubPlugins (pluginName: string, wnd: any = window): Function[] {
+export function getSubPlugins (pluginName: string, wnd: any = window): PluginConstructor[] {
   if (!wnd.hyper_plugin_extend) {
     throw new Error('No parent or sub plugins have been registered.')
   }
@@ -73,5 +77,3 @@ function getSubPlugins (pluginName: string, wnd: any = window): Function[] {
 
   return wnd.hyper_plugin_extend[pluginName]
 }
-
-export { registerSubPlugin, registerParentPlugin, getSubPlugins }
